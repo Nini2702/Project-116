a7c6f16879ef06678fbff2588aa94e64ee9f5d95
Nini2702 <82361495+Nini2702@users.noreply.github.com>
2023-11-03 21:19:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    moustache = loadImage('https://i.postimg.cc/QCpsr5Z1/7-2-moustache-png-image.png');
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+function draw(){
+    image(video,0,0,300,300);
+    image(moustache, noseX -25 , noseY + 5 , 55, 13);
+}
+
+function take_snapshot(){
+    save('myImage.png')
+}
